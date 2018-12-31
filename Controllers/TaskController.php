b340d50cfba5d817dc43278b92cbe3c75674581d
MichaelTichy22 <st52567@student.upce.cz>
2018-12-31 16:51:55 +0100

    Add task

---
+++ Controllers/TaskController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michael
+ * Date: 31.12.18
+ * Time: 16:22
+ */
+
+class TaskController extends Controller
+{
+
+}
---
+++ Tables/TaskTable.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michael
+ * Date: 31.12.18
+ * Time: 16:25
+ */
+
+class TaskTable extends Table
+{
+
+    function build(DatabaseManager $db = null)
+    {
+        $this->addColumn('id_task', 'ID');
+        $this->addColumn('name', 'Název');
+        $this->addColumn('description', 'Popis');
+        $this->addColumn('hours_done', 'Odpracované hodiny');
+        $this->addColumn('state', 'Stav');
+        $this->addColumn('description', 'Popis');
+        $this->addColumn('id_user', 'Zaměstnanec');
+        $this->addColumn('id_order', 'Zákazník');
+    }
+}
